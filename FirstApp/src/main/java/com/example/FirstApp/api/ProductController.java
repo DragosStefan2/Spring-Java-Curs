13886bff8651c57b9a5b1bd310aca990b6f9eb85
DragosStefan2 <dragos.stefan2003@yahoo.com>
2024-05-02 21:26:07 +0300

    Curs 29

--- FirstApp/src/main/java/com/example/FirstApp/api/ProductController.java
+++ FirstApp/src/main/java/com/example/FirstApp/api/ProductController.java
@@ -1,4 +1,21 @@
 package com.example.FirstApp.api;
 
-public class ProductController {
-}
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+    @RestController
+    @RequestMapping("/api/product")
+    public class ProductController {
+
+        @GetMapping("/test")
+        public String productTest() {
+            return "<h1>test1234</h1>";
+        }
+
+        @GetMapping
+        public String productString() {
+            return "product String";
+        }
+
+    }
